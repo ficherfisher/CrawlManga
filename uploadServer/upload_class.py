62a6fc00eeec244e678a0ca80d602a6d1563cd47
peng <2920151643@qq.com>
2022-08-22 17:20:55 +0800

    initial uploadServer

---
+++ uploadServer/upload_class.py
@@ -0,0 +1,94 @@
+import os
+import re
+import time
+import paramiko
+from tqdm import tqdm
+
+
+class ParamikoFolderUploader:
+    def __init__(self, host, port, user, password, local_dir: str, remote_dir: str,
+                 path_pattern_exluded_tuple=('/.git/', '/.idea/'),
+                 file_suffix_tuple_exluded=('.pyc', '.log', '.gz', '.txt'),
+                 only_upload_within_the_last_modify_time=3650 * 24 * 60 * 60,
+                 file_volume_limit=1000 * 1000, ):
+        """
+        :param path_pattern_exluded_tuple: 命中了这些正则的直接排除
+        :param file_suffix_tuple_exluded: 这些结尾的文件排除
+        :param only_upload_within_the_last_modify_time: 仅仅上传最近多少天修改的文件
+        :param file_volume_limit: 大于这个体积的不上传，单位b。
+        """
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+
+        self._local_dir = str(local_dir).replace('\\', '/')
+        if not self._local_dir.endswith('/'):
+            self._local_dir += '/'
+        self._remote_dir = str(remote_dir).replace('\\', '/')
+        if not self._remote_dir.endswith('/'):
+            self._remote_dir += '/'
+        self._path_pattern_exluded_tuple = path_pattern_exluded_tuple
+        self._file_suffix_tuple_exluded = file_suffix_tuple_exluded
+        self._only_upload_within_the_last_modify_time = only_upload_within_the_last_modify_time
+        self._file_volume_limit = file_volume_limit
+
+        t = paramiko.Transport((host, port))
+        t.connect(username=user, password=password)
+        self.sftp = paramiko.SFTPClient.from_transport(t)
+
+        ssh = paramiko.SSHClient()
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, port=port, username=user, password=password, compress=True)
+        self.ssh = ssh
+
+    def _judge_need_filter_a_file(self, filename: str):
+        ext = filename.split('.')[-1]
+        if '.' + ext in self._file_suffix_tuple_exluded:
+            return True
+        for path_pattern_exluded in self._path_pattern_exluded_tuple:
+            if re.search(path_pattern_exluded, filename):
+                return True
+        file_st_mtime = os.stat(filename).st_mtime
+        volume = os.path.getsize(filename)
+        if time.time() - file_st_mtime > self._only_upload_within_the_last_modify_time:
+            return True
+        if volume > self._file_volume_limit:
+            return True
+        return False
+
+    def _make_dir(self, dir, final_dir):
+        try:
+            self.sftp.mkdir(dir)
+            if dir != final_dir:
+                self._make_dir(final_dir, final_dir)
+        except (FileNotFoundError,):
+            parrent_dir = os.path.split(dir)[0]
+            self._make_dir(parrent_dir, final_dir)
+
+    def _sftp_put(self, file_full_name, remote_full_file_name):
+        self.sftp.put(file_full_name, remote_full_file_name)
+
+    def upload(self):
+        for parent, dirnames, filenames in os.walk(self._local_dir):
+            for filename in tqdm(filenames, desc=" ".join(parent.split("/")[-2:])):
+                file_full_name = os.path.join(parent, filename).replace('\\', '/')
+                if not self._judge_need_filter_a_file(file_full_name):
+                    remote_full_file_name = re.sub(f'^{self._local_dir}', self._remote_dir, file_full_name)
+                    try:
+                        self._sftp_put(file_full_name, remote_full_file_name)
+                    except (FileNotFoundError,) as e:
+                        self._make_dir(os.path.split(remote_full_file_name)[0], os.path.split(remote_full_file_name)[0])
+                        self._sftp_put(file_full_name, remote_full_file_name)
+                    os.remove(file_full_name)  # 删除文件
+        source_update_inf_path = "update_inf.json"
+        target_inf_path = "/home/admin/cartoon/cartoon-cat-server-master/public/userinfo.json"
+        self._sftp_put(source_update_inf_path, target_inf_path)
+
+
+if __name__ == '__main__':
+    source_ = r"D:\programmeProject\pycharmProject\scrapy\maoflymanhua\maoflymanhua\images\落第骑士的英雄谭"
+    target_ = r"/home/admin/cartoon/cartoon-cat-server-master/public/store/落第骑士英雄谭"
+    uploader = ParamikoFolderUploader('47.111.9.53', 22, 'root', '2920151643xyp?', source_, target_)
+    uploader.upload()
---
+++ uploadServer/upload_server.py
@@ -0,0 +1,91 @@
+import shutil
+
+import yaml
+import time
+import os
+import datetime
+import paramiko
+from upload_class import ParamikoFolderUploader
+
+
+class connect_server:
+    def __init__(self, host, port, user, password):
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+        t = paramiko.Transport((host, port))
+        t.connect(username=user, password=password)
+        self.sftp = paramiko.SFTPClient.from_transport(t)
+
+    def mkdir(self, remote_full_file_name):
+        try:
+            self.sftp.mkdir(remote_full_file_name)
+        except Exception as e:
+            print(e)
+
+
+def load_config():
+    fp = open("config.yaml", encoding="utf-8")
+    return yaml.load(fp.read(), Loader=yaml.FullLoader)
+
+
+def judge_upload(file_path=r"D:\programmeProject\pycharmProject\scrapy\maoflymanhua\maoflymanhua\images\ONE PIECE航海王"):
+    dir_time = os.path.getctime(file_path)
+    today = datetime.datetime.now()
+    offset = datetime.timedelta(days=-3)
+    re_date = (today + offset)
+    re_date_unix = time.mktime(re_date.timetuple())
+    if dir_time <= re_date_unix:
+        return False
+    else:
+        return True
+
+
+def _del_all_file(tmp, delDir):
+    for filePath in delDir:
+        tmp_file = os.path.join(tmp, filePath)
+        if len(os.listdir(tmp_file)) == 0:
+            shutil.rmtree(tmp_file, True)
+
+
+def upload(file_path):
+    mangas_path = os.listdir(file_path)
+    change_dict = load_config()
+    server_path = "/home/admin/cartoon/cartoon-cat-server-master/public/store/"
+    for i in mangas_path:
+        source_ = os.path.join(file_path, i)
+        if judge_upload(source_):
+            if i not in change_dict:
+                change_dict[i] = i
+                connect_server1 = connect_server('47.111.9.53', 22, 'root', '2920151643xyp?')
+                connect_server1.mkdir(server_path + change_dict[i])
+            target_ = server_path + change_dict[i]
+            uploader = ParamikoFolderUploader('47.111.9.53', 22, 'root', '2920151643xyp?', source_, target_)
+            uploader.upload()
+            _del_all_file(source_, os.listdir(source_))
+        fp_yaml_config = open(os.path.join(os.path.dirname(__file__), "config.yaml"), "w", encoding="utf-8")
+        yaml.safe_dump(change_dict, fp_yaml_config, encoding="utf-8", allow_unicode=True)
+        fp_yaml_config.close()
+
+
+def produce_index(file_path):
+    config_path = os.path.join(file_path, "config.yaml")
+    fp = open(config_path, encoding="utf-8")
+    config_dict = yaml.load(fp.read(), Loader=yaml.FullLoader)
+    for key, value in zip(list(config_dict.keys()), list(config_dict.values())):
+        key_path = os.path.join(file_path, "images/"+key+"/index")
+        fp = open(key_path, "w")
+        for j in range(0, value):
+            fp.write(str(j+1)+"\n")
+        fp.close()
+        print("写入"+key+":index文件")
+
+
+if __name__ == '__main__':
+    upload_list = [r"D:\programmeProject\pycharmProject\scrapy\maoflymanhua\maoflymanhua",
+                   r"D:\programmeProject\pycharmProject\scrapy\xingqiumanhua\xingqiumanhua"]
+    for i in upload_list:
+        produce_index(i)
+        upload(os.path.join(i, "images"))
+
